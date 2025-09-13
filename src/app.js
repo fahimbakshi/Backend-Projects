3730bcac2e83af693c9a1400d6c88cb8473170e9
fahimbakshi <fahim.s.bakshi06@gmail.com>
2025-09-13 12:16:36 +0530

    miner changes in files

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 
-app.use(express.urlencoded({extended :true , limit :"16kb"})) //making another cnfigure, which form ,when data coms from url
+app.use(express.urlencoded({extended :true , limit :"16kb"})) //making another cnfigure, which from ,when data coms from url
 app.use(express.static("public"))//from saving files , image,etc "public" is aur folder which stores that data   
 
 app.use(cookieParser())//to access the cookies of user by server
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error{
         this.statuscode = statuscode
         this.data = null
         this.message = message
-        this.success =false
+        this.success =false  //success=false because we are handling errors
         this.errors =errors
 
         if(stack){   //this used to track the error,and shor error poroparly 
