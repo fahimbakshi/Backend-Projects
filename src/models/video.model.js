5b375d47a61515f8a8caa70ab5a76b89febae081
fahimbakshi <fahim.s.bakshi06@gmail.com>
2025-09-13 16:45:32 +0530

    afglasdjkf

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,51 +1,49 @@
-import mongoose,{schema} from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; 
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const VideoSchema =new mongoose.Schema(
-    {
-        videoFile:{
-            type:String,// we get this from cloudinary url
-            required:true
-        },
-        thumnale:{
-            type:String,// we get this from cloudinary url
-            required:true
-        },
-        title:{
-            type:String,
-            required:true
-        },
-        description:{
-            type:String,
-            required:true
-        },
-        duration:{     //cloudnary url
-            type:Number, 
-            required:true
-        },
-        view:{
-            type:Number,
-            default:0
-        },
-        isPublished:{
-            type:Boolean,
-            default:true
-        },
-        owner:{
-            type:schema.Types.objectId,
-            ref:"User"
-        }
+// Define schema
+const VideoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // Cloudinary URL
+      required: true,
     },
-    {
-       timestamps:true
-    }
-) 
+    thumbnail: {
+      type: String, // Cloudinary URL (corrected spelling from "thumnale")
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    view: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-//using mongoose aggregate hear
-//in mongoose documantion we use mongoose plugins in aur prog
+// Apply pagination plugin
+VideoSchema.plugin(mongooseAggregatePaginate);
 
-VideoSchema.plugin(mongooseAggregatePaginate)
-
-export const Video = mongoose.model("Video",VideoSchema)
-
-
+// Export model
+export const Video = mongoose.model("Video", VideoSchema);
